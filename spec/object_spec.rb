23182fb6da13c5997e0d7be0c8074c616b7a6ee4
tomhuda <tomhuda@strobecorp.com>
2011-08-15 00:18:28 -0700

    Add a test for nested objects and nested object lookup

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -3,5 +3,6 @@ require "spec_helper"
 describe "Objects" do
   it "creates a simple object" do
     e("x = { a: 1 }; x.a").should == 1
+    e("x = { a: { b: 2 }, c: 5 }; x.a.b + x.c").should == 7
   end
 end
